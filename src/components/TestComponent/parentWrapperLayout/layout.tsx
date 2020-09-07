f1900d5f160210bde5be09d9df4f8c8c07c8d027
mcalixte <mike.calixte@yahoo.ca>
2020-09-06 20:10:24 -0400

    added remaing folder structure

--- src/components/TestComponent/parentWrapperLayout/layout.tsx
+++
@@ -1,9 +0,0 @@
-import {View} from 'react-native';
-import React from 'react';
-import styles from './layoutStyles';
-
-const layout = ({children}) => {
-  return <View style={styles.wrapper}>{children}</View>;
-};
-
-export default layout;
