cdce2a8736810f600dfa042ba290f67f0dd5ce33
mcalixte <mike.calixte@yahoo.ca>
2020-09-10 16:51:04 -0400

    added code to index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import {AppRegistry} from 'react-native';
+import React from 'react';
+import {Component} from 'react';
+import {View} from 'react-native';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+AppRegistry.registerComponent(App, () => App);
