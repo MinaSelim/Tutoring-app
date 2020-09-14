8d29b231d055c15c4c5b3b16b145fa5a1912cca4
mcalixte <mike.calixte@yahoo.ca>
2020-09-14 14:52:41 -0400

    renamed project to GoStudy

--- react-native/android/app/src/main/java/com/gostudy/MainActivity.java
+++ react-native/android/app/src/main/java/com/gostudy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tutorhub;
+package com.gostudy;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "TutorHub";
+    return "GoStudy";
   }
 }
