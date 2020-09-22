96fbd73f39704d13d08dba6718c5ddca64e3c86b
SandraRoz <Bella.roz22@gmail.com>
2020-09-22 19:08:58 -0400

    added coverate utilities for testing

--- react-native/index.tsx
+++ react-native/index.tsx
@@ -4,17 +4,16 @@ import {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class App extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-    }
-
-    render(){
-        return(
-            <View>
-                <Text>Hello World!</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
 }
 AppRegistry.registerComponent('GoStudy', () => App);
