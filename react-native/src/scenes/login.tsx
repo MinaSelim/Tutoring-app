ed5bd299c54e61cfd0f847952ff9744d73f323c3
Mina Selim <supermina55555@hotmail.com>
2020-10-04 23:33:06 -0400

    Added first iteration of the code, Server side is complete

---
+++ react-native/src/scenes/login.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+class login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
+
