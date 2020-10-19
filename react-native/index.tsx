c0b29af64527076d4666f2eaa13674d3fb897846
SandraRoz <Bella.roz22@gmail.com>
2020-10-19 13:36:22 -0400

    #20 #57 revert react-native changes from commit

--- react-native/index.tsx
+++ react-native/index.tsx
@@ -2,7 +2,6 @@ import {AppRegistry} from 'react-native';
 import React from 'react';
 import {Component} from 'react';
 import {View, Text} from 'react-native';
-import {SERVER_LINK} from 'react-native-dotenv-milkywire';
 
 class App extends Component {
   constructor(props) {
