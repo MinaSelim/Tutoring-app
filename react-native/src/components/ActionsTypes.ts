cced116b2a4b4f8aefc097640050ed4ccaf02c51
Felix Morin <f_mori@live.concordia.ca>
2020-10-21 03:47:13 -0400

    Remove Redux files

--- react-native/src/components/ActionsTypes.ts
+++
@@ -1,9 +0,0 @@
-export const SET_INPUT_EMAIL = "set_input_email";
-export const SET_INPUT_PASSWORD = "set_input_password";
-export const SET_PASSWORD = "set_password";
-export const SET_FIRST_NAME = "set_first_name";
-export const SET_LAST_NAME = "set_last_name";
-export const SET_EMAIL = "set_email";
-export const SET_PHONE = "set_phone";
-export const SET_USER_TYPE = "set_user_type";
-export const SET_UNIVERSITY = "set_university";
--- react-native/src/components/SignIn/SignInAction.ts
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux';
-import SignIn from './SignIn';
-
-const mapStateToProps = (state) => ({
-    inputEmail: state.SignInReducer.inputEmail,
-    inputPassword: state.SignInReducer.inputPassword
-});
-
-export default connect(mapStateToProps)(SignIn);
