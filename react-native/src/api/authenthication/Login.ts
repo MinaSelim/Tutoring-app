991c67b88104c1610f9cb4bc1dd1855d31765b85
carlitalabbe <labbecarlita@gmail.com>
2020-10-23 09:24:52 -0400

    testing the pre-commit hook: (temp) commented out all contents of Login.ts

--- react-native/src/api/authenthication/Login.ts
+++ react-native/src/api/authenthication/Login.ts
@@ -1,51 +1,42 @@
-/* eslint-disable no-return-await */
-/* eslint-disable no-shadow */
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-import {auth} from 'firebase';
-import {SERVER_LINK} from 'react-native-dotenv-milkywire';
-import fire from './Fire';
-import IUserLogin from '../../model/IUserLogin';
+// import {auth} from 'firebase';
+// import {SERVER_LINK} from 'react-native-dotenv-milkywire';
+// import fire from './Fire';
+// import IUserLogin from '../../model/IUserLogin';
 
 /**
  * this class provides api abstraction for firebase
  */
 export default class Login {
-  private auth: auth.Auth;
-
-  constructor(auth: auth.Auth = fire.auth()) {
-    this.auth = auth;
-  }
-
-  /**
-   * Abstraction provided to communicate with firebase to authenthicate the user. it returns user information from the backend
-   * @param loginInfo the username and password of the user
-   */
-  public signInWithEmailAndPassword = async (loginInfo: IUserLogin) => {
-    await this.auth.signInWithEmailAndPassword(
-      loginInfo.email,
-      loginInfo.password,
-    );
-    return await this.signInWithServer();
-  };
-
-  /**
-   * this method communicates with the backend, sending an auth token to the server so it could authenthicate the user
-   */
-  private signInWithServer = async () => {
-    const user = this.auth.currentUser;
-    const token = user && (await user.getIdToken());
-
-    const res = await fetch(`${SERVER_LINK}/auth/login`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({idtoken: token}),
-      credentials: 'include',
-    }).then((response) => response.json());
-
-    return res;
-  };
+  // private auth: auth.Auth;
+  // constructor(auth: auth.Auth = fire.auth()) {
+  //   this.auth = auth;
+  // }
+  // /**
+  //  * Abstraction provided to communicate with firebase to authenticate the user. it returns user information from the backend
+  //  * @param loginInfo the username and password of the user
+  //  */
+  // public signInWithEmailAndPassword = async (loginInfo: IUserLogin) => {
+  //   await this.auth.signInWithEmailAndPassword(
+  //     loginInfo.email,
+  //     loginInfo.password,
+  //   );
+  //   return await this.signInWithServer();
+  // };
+  // /**
+  //  * this method communicates with the backend, sending an auth token to the server so it could authenticate the user
+  //  */
+  // private signInWithServer = async () => {
+  //   const user = this.auth.currentUser;
+  //   const token = user && (await user.getIdToken());
+  //   const res = await fetch(`${SERVER_LINK}/auth/login`, {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({idtoken: token}),
+  //     credentials: 'include',
+  //   }).then((response) => response.json());
+  //   return res;
+  // };
 }
