6c198df13030b7d3f5759833bc04cc7d473255d7
carlitalabbe <labbecarlita@gmail.com>
2020-10-22 22:15:10 -0400

    removed js / jsx from the files checked by linter

--- react-native/.eslintrc.js
+++ react-native/.eslintrc.js
@@ -2,10 +2,7 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'airbnb', 'airbnb/hooks', 'prettier'],
   rules: {
-    'react/jsx-filename-extension': [
-      2,
-      {extensions: ['.js', '.jsx', '.ts', '.tsx']},
-    ],
+    'react/jsx-filename-extension': [2, {extensions: ['.ts', '.tsx']}],
     'react/prefer-stateless-function': 'off',
     'no-useless-constructor': 'warn',
     'no-unused-vars': 'warn',
