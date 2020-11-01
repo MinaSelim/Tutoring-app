884372e1c0ec4bb3333b50a9a174d4ea0f21fe9e
Mina Selim <supermina55555@hotmail.com>
2020-10-31 21:31:24 -0400

    Added connection code to the Front-end

---
+++ react-native/src/api/authenthication/TutorAuth.ts
@@ -0,0 +1,74 @@
+import fire from './Fire';
+import {auth} from 'firebase';
+import IUserLogin from '../../model/IUserLogin';
+import {SERVER_LINK} from 'react-native-dotenv-milkywire';
+import IAuth from './IAuth';
+import ITutor from '../../model/ITutor';
+
+/**
+ * this class provides api abstraction for firebase
+ */
+export default class TutorAuth implements IAuth {
+  private auth: auth.Auth;
+
+  constructor(auth: auth.Auth = fire.auth()) {
+    this.auth = auth;
+  }
+
+  /**
+   * Abstraction provided to communicate with firebase to authenthicate the user. it returns user information from the backend
+   * @param loginInfo the username and password of the user
+   */
+  public signInWithEmailAndPassword = async (loginInfo: IUserLogin) : Promise<ITutor> => {
+    await this.auth.signInWithEmailAndPassword(
+      loginInfo.email,
+      loginInfo.password,
+    );
+    return await this.signInWithServer();
+  };
+
+    /**
+     * this function registers the student and stores them in server db and the firebase auth API.
+     *@param loginInfo the username and password of the user
+     *@param tutor the tutor that needs to be registered with the server. The firebase UID can be left empty.
+     */
+  public registerWithEmailAndPassword = async (loginInfo: IUserLogin, tutor : ITutor) : Promise<void> => {
+    try {
+      const result = await this.auth.createUserWithEmailAndPassword(loginInfo.email, loginInfo.password);
+      tutor.firebase_uid = result.user.uid;
+      let response = fetch((SERVER_LINK + "/auth/tutor/register"), {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(tutor)
+      });
+      
+    } catch (error) {
+      console.log(error)
+    }
+
+  };
+
+  /**
+   * this method communicates with the backend, sending an auth token to the server so it could authenthicate the user
+   */
+  private signInWithServer = async () => {
+    const user = this.auth.currentUser;
+    const token = user && (await user.getIdToken());
+
+    const res = await fetch(SERVER_LINK + '/auth/tutor/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({idtoken: token}),
+      credentials: 'include',
+    })
+    
+    const responseInJson = await res.json();
+    return responseInJson;
+  };
+}
