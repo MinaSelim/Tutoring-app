0259e44e8dd1e246813782f68a32a8b1225cfcac
Felix Morin <f_mori@live.concordia.ca>
2020-12-06 01:15:35 -0500

    basic top menu

--- react-native/src/components/myChats/MyChats.tsx
+++ react-native/src/components/myChats/MyChats.tsx
@@ -1,22 +1,66 @@
-import {View, TouchableOpacity, Image} from 'react-native';
-import 'react-native-gesture-handler';
-import {Text} from '@ui-kitten/components';
-import React from 'react';
+/* eslint-disable */
 
-const MyChats = (props): JSX.Element => {
-  return (
-    <View>
-      <TouchableOpacity
-        style={{position: 'absolute'}}
-        onPress={(): boolean => props.navigation.goBack()}>
-        <Image
-          source={require('../../assets/images/icons/backBtn.png')}
-          // style={styles.goBackButton}
-        />
-      </TouchableOpacity>
-      <Text>My Chats: Coming Soon!</Text>
-    </View>
-  );
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {TabBar, Tab, Layout, Text, Button, Icon} from '@ui-kitten/components';
+
+const {Navigator, Screen} = createMaterialTopTabNavigator();
+
+const MyChats = () => {
+    return(
+     <Layout style={{flex:1}}>
+        <ChatHeader/>
+        <ChatMenu/>
+     </Layout>
+    );
 };
+
+const ChatHeader = () => (
+  <Layout style={{flexDirection: 'row', justifyContent:'space-between'}}>
+    <Button
+          appearance="ghost"
+          accessoryLeft={BackButton}
+        />
+    <Text style={{alignSelf:'center', top: 5, fontSize: 20, fontWeight: 'bold'}}>My Chats</Text>
+    <Text style={{alignSelf:'center', right: 30, top: 5}}>(Button)</Text>
+  </Layout>
+);
+
+const BackButton = () => {
+  return <Icon fill="black" name="arrow-back-outline" style={{alignSelf:'center',
+    width: 50,
+    height: 40,
+    left: 3,
+    top: 5,
+}}/>;
+};
+
+const ChatMenu = () => (
+  <Navigator tabBar={(props) => <TopTabBar {...props} />}>
+    <Screen name="Tutors" component={Tutors} />
+    <Screen name="StudyGroups" component={StudyGroups} />
+  </Navigator>
+);
+
+const TopTabBar = ({navigation, state}) => (
+  <TabBar
+    selectedIndex={state.index}
+    onSelect={(index) => navigation.navigate(state.routeNames[index])}>
+    <Tab title="Tutors" />
+    <Tab title="Study Groups" />
+  </TabBar>
+);
+
+const Tutors = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">Tutors</Text>
+  </Layout>
+);
+
+const StudyGroups = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category="h1">Study Groups</Text>
+  </Layout>
+);
 
 export default MyChats;
