dccbae7057d92c136f573010ed2a112024dc8624
carlitalabbe <labbecarlita@gmail.com>
2020-12-26 17:50:34 -0500

    side menu button set up

--- react-native/src/components/sideBar/SideBar.tsx
+++ react-native/src/components/sideBar/SideBar.tsx
@@ -1,13 +1,96 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable react/jsx-props-no-spreading */
 import {View} from 'react-native';
 import 'react-native-gesture-handler';
-import {Text} from '@ui-kitten/components';
+import {
+  Text,
+  Layout,
+  Avatar,
+  Button,
+  Icon,
+  Divider,
+} from '@ui-kitten/components';
 import React from 'react';
+import styles from './styles/styles';
+
+const PersonIcon = (props) => <Icon {...props} name="person-outline" />;
+const CalendarIcon = (props) => <Icon {...props} name="calendar-outline" />;
+const CreditCardIcon = (props) => <Icon {...props} name="credit-card" />;
+const HistoryIcon = (props) => <Icon {...props} name="archive-outline" />;
+const CampusIcon = (props) => <Icon {...props} name="home-outline" />;
+const EmptyIcon = (props) => (
+  <Icon {...props} name="code-outline" fill="#ffffff00" />
+);
 
 const SideBar = (): JSX.Element => {
   return (
-    <View>
-      <Text>SideBar: Coming Soon!</Text>
-    </View>
+    <Layout level="primary" style={styles.container}>
+      <Layout level="primary" style={styles.userTitleContainer}>
+        <Text style={styles.text}>User Name</Text>
+        <Avatar
+          size="giant"
+          source={require('./avatar_placeholder.png')}
+          style={styles.avatar}
+        />
+      </Layout>
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={PersonIcon}
+        size="giant">
+        Profile
+      </Button>
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={CalendarIcon}
+        size="giant">
+        My Sessions
+      </Button>
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={CreditCardIcon}
+        size="giant">
+        Payment Options
+      </Button>
+      <Divider style={styles.divider} />
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={HistoryIcon}
+        size="giant">
+        Account History
+      </Button>
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={CampusIcon}
+        size="giant">
+        Select Campus
+      </Button>
+      <Divider style={styles.divider} />
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="control"
+        accessoryLeft={EmptyIcon}
+        size="giant">
+        Sign out
+      </Button>
+      <Button
+        style={styles.termsButton}
+        appearance="ghost"
+        status="control"
+        size="medium">
+        Terms and Conditions
+      </Button>
+    </Layout>
   );
 };
 
---
+++ react-native/src/components/sideBar/styles/styles.tsx
@@ -0,0 +1,38 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  text: {
+    fontSize: 24,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    marginRight: 20,
+  },
+  userTitleContainer: {
+    paddingTop: 120,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 27,
+  },
+  avatar: {
+    width: 66,
+    height: 66,
+  },
+  button: {
+    justifyContent: 'flex-start',
+    fontSize: 18,
+    fontWeight: '800',
+  },
+  divider: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  termsButton: {
+    alignSelf: 'center',
+  },
+});
+export default styles;
