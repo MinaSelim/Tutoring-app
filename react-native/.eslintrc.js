1741302230f7f03e29a3472189920b1c8354f9eb
Louis-Benjamin <lbduro9012@gmail.com>
2020-12-29 18:23:09 -0500

    Removed 'react/jsx-curly-newline' to 'off'

--- react-native/.eslintrc.js
+++ react-native/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
-    'react/jsx-curly-newline': 'off',
     '@typescript-eslint/explicit-function-return-type': 'warn',
     'react/jsx-props-no-spreading': 'warn',
     'no-unused-expressions': ['error', {allowTernary: true}],
