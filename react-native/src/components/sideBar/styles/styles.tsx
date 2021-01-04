97a01597360f8e8c222ddfd1f6c8c8280cede7b8
carlitalabbe <labbecarlita@gmail.com>
2021-01-03 19:43:40 -0500

    removed reducer logic

--- react-native/src/components/sideBar/styles/styles.tsx
+++ react-native/src/components/sideBar/styles/styles.tsx
@@ -34,5 +34,10 @@ const styles = StyleSheet.create({
   termsButton: {
     alignSelf: 'center',
   },
+  terms:{
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 8,
+  }
 });
 export default styles;
