f553615a534b729cb93d9594d767502865707166
patricia nunes <pat.nunes94@gmail.com>
2021-01-06 15:05:03 -0500

    Fixed JavaDoc comments

--- react-native/src/api/chatroom/DirectMessageChat.ts
+++ react-native/src/api/chatroom/DirectMessageChat.ts
@@ -4,9 +4,9 @@ import constants from './chatConstants';
 
 export default class DirectMessageChat extends GenericChat {
   /**
-   * Creates a groupchat with multiple users (more than 2)
+   * Creates a direct message chat with only two users
    * @param currentUserToken The firebase UID of the current logged in user
-   * @param participantsTokens An array of users firebase UID's
+   * @param participantsTokens Another user's firebase UID
    * @param roomName The name of the chatroom
    */
   public createChatroom = (
