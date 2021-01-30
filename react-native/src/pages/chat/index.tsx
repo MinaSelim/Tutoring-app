c34c391c430e70d08faf9a49cddf100257a3cb26
patricia nunes <pat.nunes94@gmail.com>
2021-01-29 20:34:46 -0500

    Cleaned up code

--- react-native/src/pages/chat/index.tsx
+++ react-native/src/pages/chat/index.tsx
@@ -1,20 +1,17 @@
 import React, {useEffect, useState, useRef, useCallback} from 'react';
 import firebase from '../../api/authentication/Fire';
-import {FlatList, PushNotificationIOS} from 'react-native';
+import {FlatList, SectionList, Text, View} from 'react-native';
 import 'react-native-gesture-handler';
-import {useStyleSheet, Layout, Input} from '@ui-kitten/components';
+import {useStyleSheet, Layout} from '@ui-kitten/components';
 import {chatStyles} from '../../components/ChatUI/styles/chatStyles';
 import MessageRow from '../../components/ChatUI/MessageRow';
 import IMessage from '../../model/IMessage';
-import DATA from '../../components/ChatUI/DATA';
 import ChatHeader from '../../components/ChatUI/ChatHeader';
 import ChatInput from '../../components/ChatUI/ChatInput';
 import GenericChat from '../../api/chatroom/GenericChat';
-import {
-  useCollectionData,
-  useCollectionDataOnce,
-} from 'react-firebase-hooks/firestore';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
 import Message from '../../api/chatroom/components/Message';
+import moment from 'moment';
 // This is the main front end for the chat, it calls messageRow for the layout of every single message view
 // and uses placeholder data from DATA.tsx to display messages, for prototyping.
 
@@ -41,15 +38,8 @@ const loadMessages = (
 const Chat = (): JSX.Element => {
   const UPDATE_MESSAGE_COUNT = 1;
   const SCROLL_MESSAGE_AMOUNT = 10;
-  const initialLoad = true;
   const [chatMessages, setChatMessages] = useState<Message[]>([]);
   const [offset, setOffset] = useState(new Date().getTime());
-  const [recentMessage, setRecentMessage] = useState<IMessage>({
-    id: '',
-    content: '',
-    createdAt: 0,
-    sender: '',
-  });
 
   const handleOnEndReached = (): void => {
     let chatLength = chatMessages.length;
@@ -59,15 +49,6 @@ const Chat = (): JSX.Element => {
   };
 
   const GetMessages = useCallback<() => Promise<Message[]>>(async () => {
-    // console.log('on end reached');
-    // const [
-    //   messages,
-    //   isLoadingAllMesages = true,
-    //   AllMesagesLoadError,
-    // ] = useCollectionDataOnce(loadMessages(SCROLL_MESSAGE_AMOUNT, OFFSET), {
-    //   idField: 'id',
-    // });
-    // return messages;
     const tempMessages: Message[] = await chatAPI.loadMessages(
       chatID,
       userID,
@@ -76,18 +57,13 @@ const Chat = (): JSX.Element => {
     );
     console.log(tempMessages);
     return tempMessages;
-    // .then((res) => {
-    //   let messages = ChatMessages(res);
-    //   chatMessages = [...(chatMessages || []), ...messages];
-    //   console.log('chatmessages', chatMessages);
-    //   return chatMessages;
-    // });
   }, [offset]);
 
   useEffect(() => {
     let tempMessages;
     GetMessages().then((res) => {
       tempMessages = res;
+      initialLoad = false;
       setChatMessages([...(chatMessages || []), ...tempMessages]);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -103,17 +79,17 @@ const Chat = (): JSX.Element => {
 
   if (newestMessage !== undefined && chatMessages[0] !== undefined) {
     const newestMsg: IMessage[] = ChatMessages(newestMessage);
-    //a new message has been received but there are no previous messages to load
+    //execute if a new message has been received
     if (chatMessages[0].id !== newestMsg[0].id)
       chatMessages.unshift(...newestMsg);
   }
+  console.log('initialLoad', initialLoad);
 
   const renderMessage = ({item}): JSX.Element => (
     <MessageRow key={item.id} {...item} />
   );
 
   const styles = useStyleSheet(chatStyles);
-  //TODO: Make it scroll when opening input boxloadMessages(SCROLL_MESSAGE_AMOUNT, offset)
   const flatListRef = useRef<null | FlatList<IMessage>>(null);
 
   function scrollToBottom(): void {
@@ -122,19 +98,22 @@ const Chat = (): JSX.Element => {
   }
   return (
     <Layout style={styles.container}>
-      <ChatHeader />
-      {!false && (
-        <FlatList<IMessage>
-          keyExtractor={(item): string => item.id}
-          renderItem={renderMessage}
-          data={chatMessages}
-          onEndReachedThreshold={0.5}
-          onEndReached={handleOnEndReached}
-          ref={flatListRef}
-          inverted
-        />
+      {!initialLoad && (
+        <>
+          <ChatHeader />
+          <FlatList<any>
+            keyExtractor={(item): string => item}
+            renderItem={renderMessage}
+            data={chatMessages}
+            onEndReachedThreshold={0.5}
+            onEndReached={handleOnEndReached}
+            ref={flatListRef}
+            inverted
+          />
+
+          <ChatInput onFocus={scrollToBottom} />
+        </>
       )}
-      <ChatInput onFocus={scrollToBottom} onPress={scrollToBottom} />
     </Layout>
   );
 };
