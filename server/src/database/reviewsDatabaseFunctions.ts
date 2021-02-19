a79287ce768dba10b86c7cf470a0158ad2708304
conso1e <menachemharrison@gmail.com>
2021-02-19 17:42:39 -0500

    created addReviewToDatabase function

---
+++ server/src/database/reviewsDatabaseFunctions.ts
@@ -0,0 +1,48 @@
+import IReview from '../models/IReview';
+import DatabaseUtils from '../database/databaseUtils';
+import * as dbConfig from '../config/DatabaseConfigInfo.json';
+import { PutItemInput, PutItemOutput } from 'aws-sdk/clients/dynamodb';
+
+
+export default class ReviewDatabaseFunctions {
+    private databaseUtils: DatabaseUtils;
+
+    constructor() {
+        this.databaseUtils = DatabaseUtils.getInstance();
+    }
+
+    public addReviewToDatabase = (review: IReview): Promise<PutItemOutput> => {
+        const params: PutItemInput = {
+            Item: {
+                reviewId: {
+                    S: review.studentId + review.tutorId + review.timestamp
+                },
+                studentId: {
+                    S: review.studentId
+                },
+                tutorId: {
+                    S: review.tutorId
+                },
+                reviewTest: {
+                    S: review.reviewText
+                },
+                communicationRating: {
+                    // dynamo likes to send numbers to the db as strings (don't ask)
+                    N: String(review.communicationRating)   
+                },
+                knowledgeRating: {
+                    N: String(review.knowledgeRating)
+                },
+                wouldTakeAgainRating: {
+                    N: String(review.wouldTakeAgainRating)
+                },
+                timestamp: {
+                    S: review.timestamp
+                }
+             },
+             ReturnConsumedCapacity: 'TOTAL',
+             TableName: dbConfig.tableNames.REVIEWS,
+        }
+        return this.databaseUtils.putItem(params);
+    }
+}
