b6877b306b52aa4f579419cb2839a28e5ade0a4c
conso1e <menachemharrison@gmail.com>
2021-02-19 15:36:28 -0500

    created review data model

---
+++ server/src/models/Review.ts
@@ -0,0 +1,9 @@
+export default interface IReview {
+    reviewId: string,
+    studentId: string,
+    tutorId: string,
+    reviewText: string,
+    communicationRating: number,
+    knowledgeRating: number,
+    wouldTakeAgainRating: number,
+}
