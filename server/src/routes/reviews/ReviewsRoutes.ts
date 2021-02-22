049b106d678d9b6cf4540cef5b7e2224c702da87
conso1e <menachemharrison@gmail.com>
2021-02-22 00:45:50 -0500

    replaced hardcoded user id with id taken from session token

--- server/src/routes/reviews/ReviewsRoutes.ts
+++ server/src/routes/reviews/ReviewsRoutes.ts
@@ -11,17 +11,12 @@ export class ReviewsRoutes implements IRouteComponent {
       this.reviewsManager = new ReviewsManager();
    }
 
-   /**
-    * This is the function that adds the profile routes to the function
-    * @param app the application to set routes on
-    */
    public route(app: Application): void {
       app.post('/reviews/addReview', async (req: Request, res: Response) => {
          try {
-            // Guards.loggedInStudentGuard(req);
+            Guards.loggedInStudentGuard(req);
             const review: IReview = req.body;
-            review.studentId = '123';
-            // review.studentId = req.session.firebase_uid
+            review.studentId = req.session.firebase_uid;
             await this.reviewsManager.addReview(review);
             res.sendStatus(200);
          } catch (error) {
@@ -32,7 +27,6 @@ export class ReviewsRoutes implements IRouteComponent {
 
       app.post('/reviews/getTutorReviews', async (req: Request, res: Response) => {
          try {
-            Guards.loggedInStudentGuard(req);
             const reviews: IReview[] = await this.reviewsManager.getTutorReviews(req.body.tutorId);
             res.status(200);
             res.send(reviews);
