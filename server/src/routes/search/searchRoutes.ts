8acbf29ea4ce3d08aa83f53080435dac76831731
conso1e <menachemharrison@gmail.com>
2021-03-01 21:42:44 -0500

    added get classes for university route

--- server/src/routes/search/searchRoutes.ts
+++ server/src/routes/search/searchRoutes.ts
@@ -2,6 +2,7 @@ import {Application, Request, Response} from 'express';
 import IRouteComponent from '../IRouteComponent';
 import SearchManager from '../../services/managers/SearchManager';
 import ITutor from '../../models/ITutor';
+import fs from 'fs';
 
 export class SearchRoutes implements IRouteComponent {
    private searchManager: SearchManager;
@@ -16,6 +17,28 @@ export class SearchRoutes implements IRouteComponent {
             const tutors: ITutor[] = await this.searchManager.getAllTutors();
             res.status(200);
             res.send(tutors);
+         } catch (error) {
+            res.status(500);
+            res.send(error);
+         }
+      });
+
+      app.post('/search/classes', (req: Request, res: Response): void => {         
+         try {
+            const university: string = req.body.university;
+            const filepath = './src/universityInformation/classes/' + university.toLowerCase() + '.json';
+            
+            fs.readFile(filepath, 'utf-8', (err, data) => {
+               if (err) {
+                  res.status(500);
+                  res.send(err.message);
+               } 
+               else {
+                  const classes = JSON.parse(data);
+                  res.status(200);
+                  res.send(classes.classes);
+               }
+            })
          } catch (error) {
             res.status(500);
             res.send(error);
