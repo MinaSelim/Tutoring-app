53d2e132fc271124afc37f6806ca175dc8ed844c
conso1e <menachemharrison@gmail.com>
2021-03-03 16:20:59 -0500

    implemented programmatic import of classes

--- server/src/config/UniversityInformation.ts
+++ server/src/config/UniversityInformation.ts
@@ -1,29 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import * as universitiesData from '../../universityInformation/universities.json';
 
 export default class UniversityInformation {
    private static instance: UniversityInformation;
-   private static universities: string[];
-   private static classes: Record<string, string[]>;
+   private universities: string[];
+   private classes: Record<string, string[]>;
+
+   private constructor() {
+      this.universities = universitiesData.universities;
+      this.classes = {};
+      this.universities.forEach((university) => {
+         const filepath = '../../universityInformation/classes/' + university + '.json';
+         this.classes[university] = require(filepath).classes;
+      });
+   }
 
    public static getInstance = (): UniversityInformation => {
       if (!UniversityInformation.instance) {
          UniversityInformation.instance = new UniversityInformation();
-
-         UniversityInformation.universities = universitiesData.universities;
-         UniversityInformation.universities.forEach((university) => {
-            const filepath = '../../universityInformation/classes/' + university + '.json';
-            UniversityInformation.classes[university] = require(filepath);
-         });
       }
-
       return UniversityInformation.instance;
    };
 
    public getUniversities = (): string[] => {
-      return UniversityInformation.universities;
+      return this.universities;
    };
 
    public getClasses = (university: string): string[] => {
-      return UniversityInformation.classes[university];
+      return this.classes[university];
    };
 }
