4f715c043a913b7c23cd357a54afc5e45a9b2269
patricia nunes <pat.nunes94@gmail.com>
2021-03-02 23:02:02 -0500

    WIP, tutor search bar page

--- react-native/src/components/tutorSearch/TutorSearch.tsx
+++ react-native/src/components/tutorSearch/TutorSearch.tsx
@@ -1,14 +1,61 @@
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import 'react-native-gesture-handler';
-import {Text} from '@ui-kitten/components';
+import {
+  Text,
+  Layout,
+  Autocomplete,
+  AutocompleteItem,
+  TopNavigation,
+  TopNavigationAction,
+  Icon,
+} from '@ui-kitten/components';
 import React from 'react';
+
+const BackIcon = (props): JSX.Element => <Icon {...props} name="arrow-back" />;
+
+const SearchIcon = (props): JSX.Element => (
+  <Icon {...props} name="search-outline" />
+);
+
+const renderBackAction = (): JSX.Element => (
+  <TopNavigationAction icon={BackIcon} />
+);
 
 const TutorSearch = (): JSX.Element => {
   return (
-    <View>
-      <Text>Tutor search: Coming Soon!</Text>
-    </View>
+    <Layout style={{flexDirection: 'column', flex: 1}}>
+      <Layout style={{}}>
+        <TopNavigation alignment="center" accessoryLeft={renderBackAction} />
+      </Layout>
+      <Layout
+        style={{
+          flex: 0.2,
+          backgroundColor: '#FF0000',
+          justifyContent: 'center',
+        }}>
+        <Layout style={{alignSelf: 'center'}}>
+          <Text style={TutorSearchStyles.title}>Find a Tutor</Text>
+        </Layout>
+      </Layout>
+      <Layout style={{padding: 20}}>
+        <Autocomplete
+          placeholder="Class ID (e.g. MATH206)"
+          accessoryLeft={SearchIcon}
+        />
+      </Layout>
+    </Layout>
   );
 };
+
+const TutorSearchStyles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    width: '80%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+});
 
 export default TutorSearch;
