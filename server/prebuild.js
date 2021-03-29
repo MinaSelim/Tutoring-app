3438d33ae58f3236a4b7d94e600a3bfefc52320a
Mike Calixte <mike.calixte@yahoo.ca>
2021-03-29 15:51:10 -0400

    Using proper unix commands for prebuild script (#72)
    * Using proper unix commands for prebuild script
    
    * Made prebuild script to avoid manually running prebuild

---
+++ server/prebuild.js
@@ -0,0 +1,13 @@
+var exec = require('child_process').exec;
+var os = require('os');
+
+function puts(error, stdout, stderr) { console.log(stdout) }
+
+if (os.type() === 'Linux') 
+   exec("npm run prebuild-unix", puts); 
+else if (os.type() === 'Darwin') 
+   exec("npm run prebuild-unix", puts); 
+else if (os.type() === 'Windows_NT') 
+   exec("npm run prebuild-windows", puts);
+else
+   throw new Error("Unsupported OS found: " + os.type());
